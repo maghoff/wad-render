e310d9f573269941df8d437e1faafbd0af57fcc3
Magnus Hovland Hoff <maghoff@gmail.com>
2019-08-09 16:21:13 +0200

    A crashbug appears in my wasm when building with --release and wasm-gc

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -e
 
 export RUSTFLAGS=
 
-cargo +nightly build --target wasm32-unknown-unknown --release
-wasm-gc target/wasm32-unknown-unknown/release/wad_render.wasm -o wad_render.gc.wasm
+# cargo +nightly build --target wasm32-unknown-unknown --release
+# wasm-gc target/wasm32-unknown-unknown/release/wad_render.wasm -o wad_render.gc.wasm
+
+cargo +nightly build --target wasm32-unknown-unknown
+cp target/wasm32-unknown-unknown/debug/wad_render.wasm wad_render.gc.wasm
