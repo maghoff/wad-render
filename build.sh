1532d4783ae7007e7168dfefe6df1b6f645e5208
Magnus Hovland Hoff <maghoff@gmail.com>
2019-08-09 21:10:11 +0200

    Tweak build.sh.
    Revert to --release and wasm-gc, which seem to be working now

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ set -e
 
 export RUSTFLAGS=
 
-# cargo +nightly build --target wasm32-unknown-unknown --release
-# wasm-gc target/wasm32-unknown-unknown/release/wad_render.wasm -o wad_render.gc.wasm
+cargo +nightly build --target wasm32-unknown-unknown --release
+wasm-gc target/wasm32-unknown-unknown/release/wad_render.wasm -o wad_render.gc.wasm
+# cp target/wasm32-unknown-unknown/release/wad_render.wasm wad_render.gc.wasm
 
-cargo +nightly build --target wasm32-unknown-unknown
-cp target/wasm32-unknown-unknown/debug/wad_render.wasm wad_render.gc.wasm
+# cargo +nightly build --target wasm32-unknown-unknown
+# wasm-gc target/wasm32-unknown-unknown/debug/wad_render.wasm -o wad_render.gc.wasm
+# cp target/wasm32-unknown-unknown/debug/wad_render.wasm wad_render.gc.wasm
