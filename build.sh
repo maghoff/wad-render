40eaf12a7bd34e6f490c7fb08df44157b1895596
Johannes Hoff <johshoff@gmail.com>
2020-05-25 00:13:20 +0200

    Use rust stable instead of nightly

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -e
 export RUSTFLAGS=
 export CARGO_INCREMENTAL=0
 
-cargo +nightly build --target wasm32-unknown-unknown --release
+cargo build --target wasm32-unknown-unknown --release
 wasm-gc target/wasm32-unknown-unknown/release/wad_render.wasm -o wad_render.gc.wasm
 # cp target/wasm32-unknown-unknown/release/wad_render.wasm wad_render.gc.wasm
 
-# cargo +nightly build --target wasm32-unknown-unknown
+# cargo build --target wasm32-unknown-unknown
 # wasm-gc target/wasm32-unknown-unknown/debug/wad_render.wasm -o wad_render.gc.wasm
 # cp target/wasm32-unknown-unknown/debug/wad_render.wasm wad_render.gc.wasm
